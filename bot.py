71e453962176aac0cbb7ce4c2045a53e9ac814b9
Hi Gaming DE2000 <128186675+HiGamingDE2000@users.noreply.github.com>
2023-09-04 19:09:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,13 @@ from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 activity = discord.Activity(type=discord.ActivityType.listening, name="online")
-status = discord.Status.online
+status = discord.Status.online # onlinr, idle, dnd
 
 bot = discord.Bot(
     intents=intents,
     activity=activity,
     status=status
 )
-
 
 load_dotenv()
 bot.run(os.getenv("TOKEN"))
