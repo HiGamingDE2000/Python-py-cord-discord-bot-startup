82edd9c4edac320f4910826cf357a3ee172eea0f
Hi Gaming DE2000 <128186675+HiGamingDE2000@users.noreply.github.com>
2024-02-14 12:20:58 +0100

    Update bot.py
    added ezcord

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import discord
+import ezcord
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 activity = discord.Activity(type=discord.ActivityType.listening, name="online")
 status = discord.Status.online # online, idle, dnd
 
-bot = discord.Bot(
+bot = ezcord.Bot(
     intents=intents,
     activity=activity,
     status=status
